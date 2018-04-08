c36d970caf68b2249681c480da53dbe2a03ef8c8
desfarik <desfarik@gmail.com>
2018-04-08 20:22:20 +0300

    add schedule-content

--- source/private/calendar/calendar.controller.js
+++ source/private/calendar/calendar.controller.js
@@ -1,5 +1,10 @@
 export default function CalendarController() {
     let vm = this;
-    vm.currentDate = _.startCase(moment().format("MMMM YYYY"));
     vm.currentMonth = moment().format("MMMM");
+    vm.schedule = {
+        work_days : ["пн: 8:00 - 15:00", "вт: 8:00 - 15:00", "ср: 8:00 - 15:00","чт: 8:00 - 15:00","пт: 8:00 - 15:00"],
+        day_off: ["сб,вс: выходные"]};
+
+    vm.monthsForSchedule = moment.months().slice(3,3+3).map(month=>_.startCase(month));
+    vm.selectedMonth = vm.monthsForSchedule[0];
 }
